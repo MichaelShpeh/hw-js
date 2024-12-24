a5a61d1b834fa7fe34ee8efedce5da29ee2ac5fa
Michael Shpeh <mspeg236@gmail.com>
2024-12-25 01:26:02 +0200

    extra hw to 5 hw

--- lesson-5/js/lesson-5.js
+++ lesson-5/js/lesson-5.js
@@ -357,3 +357,46 @@ if (isNaN(numberFirst) || isNaN(numberTwo)) {
     }
     alert(`Результат: ${result}`);
 }
+
+//! Додаткове завдання
+
+//? Hапиши скрипт перевірки типу змінної <emptyString1>
+//? за допомогою конструкції «if...else» або «if...else...if».
+let emptyString1;
+// emptyString1 = "";
+//  emptyString1 = " ";
+// emptyString1 = "1";
+// emptyString1 = 1;
+// emptyString1 = null;
+// emptyString1 = false;
+// emptyString1 = true;
+// emptyString1 = 0;
+// emptyString1 = "0";
+console.log("emptyString1:", emptyString1); //! 
+console.log("typeof emptyString1:", typeof emptyString1); //! string
+console.log("Number(emptyString1):", Number(emptyString1)); //! 0
+console.log("-------------------");
+
+//? Перевірка на "що це" (який тип значення)?:
+
+if (emptyString1 === "") {
+    console.log("Це пустий рядок")
+} else if (typeof emptyString1 === "number") {
+    console.log("Це число")
+} else if (emptyString1 === null) {
+    console.log("Це пустота")
+} else if (emptyString1 === false) {
+    console.log("Це не правда")
+} else if (emptyString1 === true) {
+    console.log("Це правда")
+} else if (emptyString1 == false) {
+    console.log("Це не визначене значення")
+} else if (emptyString1.trim() === "") {
+    console.log("Тут є пробіл")
+} else if (emptyString1.trim().length === Number(emptyString1)) {
+    console.log("Це рядок")
+} else {
+    console.log("Невідомий тип змінної")
+};
+
+//! Друга частина
