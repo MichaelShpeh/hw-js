a638f894f6eca5f69abc8bb2027e3f03e7a663ee
Michael Shpeh <mspeg236@gmail.com>
2025-03-26 16:47:04 +0200

    ready 14-hw

--- lesson-14/js/lesson-14.js
+++ lesson-14/js/lesson-14.js
@@ -136,7 +136,9 @@ console.log(
 );
 //? Отримати масив імен користувачів за статтю (поле gender).
 const getUsersWithGender = (users, gender) => {
-    //! твій код
+    const usersByGender = users.filter(user => user.gender === gender);
+
+    return usersByGender;
 };
 
 console.log(getUsersWithGender(users, 'male')); //! [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
@@ -151,7 +153,7 @@ console.log(
 );
 //? Отримати масив тільки неактивних користувачів (поле isActive).
 const getInactiveUsers = users => {
-    //! твій код
+    return users.filter(user => !user.isActive);
 };
 
 console.log(getInactiveUsers(users)); //! [об'єкт Moore Hensley, об'єкт Ross Vazquez, об'єкт Blackburn Dotson]
@@ -166,7 +168,9 @@ console.log(
 );
 //? Отримати користувача (не масив) по email (поле email, він унікальний).
 const getUserWithEmail = (users, email) => {
-    //! твій код
+    const usersByEmail = users.filter(user => user.email === email);
+
+    return usersByEmail;
 };
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); //! {об'єкт користувача Sheree Anthony}
