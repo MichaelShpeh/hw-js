94a2b64a0eb8c153aa8bfbdc8cf76d8d697833e4
Michael Shpeh <mspeg236@gmail.com>
2025-05-25 17:55:39 +0300

    finished hw-2

--- 4semester-lessons/lesson-2/lesson-2.js
+++ 4semester-lessons/lesson-2/lesson-2.js
@@ -7,3 +7,21 @@ const newSize = _.debounce(() => {
 }, 300);
 
 slideBar.addEventListener("input", newSize);
+
+const parentBox = document.querySelector(".parent-box");
+const box = document.getElementById("box");
+
+const movebox = _.debounce((e) => {
+  const rect = parentBox.getBoundingClientRect();
+  const boxSize = box.offsetWidth;
+  
+  let x = e.clientX - rect.left - boxSize / 2;
+  let y = e.clientY - rect.top - boxSize / 2;
+
+  x = Math.max(0, Math.min(x, rect.width - boxSize));
+  y = Math.max(0, Math.min(y, rect.height - boxSize));
+
+  box.style.transform = `translate(${x}px, ${y}px)`;
+}, 10);
+
+parentBox.addEventListener("mousemove", movebox);
